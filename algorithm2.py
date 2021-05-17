1cea43d690b1f693cec4100a7b8a84953ae7d44e
Sarvenaz <ge59rom>
2021-05-17 12:08:12 +0200

    algortihm2

--- algorithm2.py
+++ algorithm2.py
@@ -3,17 +3,27 @@
 import multiplier
 import random
 
-#x = bytes(random.randint(0, 255))
-x = b'\x0b'
-z = multiplier.sec_gf_mul(x, x)
+def inversion (x):
 
-y = multiplier.sec_gf_mul(x,z)
+    z = multiplier.sec_gf_mul(x, x)
 
-print(multiplier.sec_gf_mul(y,multiplier.sec_gf_mul(y, multiplier.sec_gf_mul(y,y))))
-print(multiplier.gf_exp(y,4))
+    y = multiplier.sec_gf_mul(x,z)
 
+    #print(multiplier.sec_gf_mul(y,multiplier.sec_gf_mul(y, multiplier.sec_gf_mul(y,y))))
+    #print(multiplier.gf_exp(y,4))
 
+    w = multiplier.gf_exp(y,4)
 
-#y = multiplier.sec_gf_mul(y,w)
+    y = multiplier.sec_gf_mul(y,w)
 
-#y = 
+    y = multiplier.gf_exp(y,16)
+
+    y = multiplier.sec_gf_mul(y,w)
+
+    y = multiplier.sec_gf_mul(y,z)
+
+    return y
+
+x = bytes(random.randint(0, 255))
+#x = b'\x0b'
+print(inversion(x))
